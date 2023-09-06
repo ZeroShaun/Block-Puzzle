a8125ec6dbdb58ee5f71e01cb975c37d53fd6b5c
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2023-09-05 23:03:38 -0500

    Tidy up & Lose
    Optimized the game a bit more and added the lose logic.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-const game = {
+let game = {
   started:false,
   upcomingBlock: {
     Position:[],
@@ -10,83 +10,77 @@ const game = {
   grid: [], // First index is the x coordinate, second is the y, then 0 = true or false and 1 = color. So grid[x][y][0] returns true or false and grid[x][y][1] returns the color.
   shapeL:{ // Default values for L shape
     Position:[
-      [[3,1],[4,1],[5,1],[5,0]], // Active position. Array structure is Position[1][2][0] -> 1 refers to first orientation, 2 refers to the second piece of the shape in that orienation and 0 points to the x coordinate.
       [[3,1],[4,1],[5,1],[5,0]], // Orientation 1
       [[4,0],[4,1],[4,2],[5,2]], // Orientation 2
       [[5,1],[4,1],[3,1],[3,2]], // Orienation 3
       [[4,2],[4,1],[4,0],[3,0]], // Orientation 4
     ], 
-    Orientation: 1, // Holds the orientation of the shape.
+    Orientation: 0, // Holds the orientation of the shape.
     Color: "rgb(224, 86, 0)", // Holds the color of the objects; used to color the grid.
   },
-  shapeJ:{ // Default values for J shape
+  shapeJ:{ 
     Position:[
-      [[3,0],[3,1],[4,1],[5,1]], // Active position. Array structure is Position[1][2][0] -> 1 refers to first orientation, 2 refers to the second piece of the shape in that orienation and 0 points to the x coordinate.
-      [[3,0],[3,1],[4,1],[5,1]], // Orientation 1
-      [[5,0],[4,0],[4,1],[4,2]], // Orientation 2
-      [[5,2],[5,1],[4,1],[3,1]], // Orienation 3
-      [[3,2],[4,2],[4,1],[4,0]], // Orientation 4
+      [[3,0],[3,1],[4,1],[5,1]], 
+      [[5,0],[4,0],[4,1],[4,2]],
+      [[5,2],[5,1],[4,1],[3,1]],
+      [[3,2],[4,2],[4,1],[4,0]],
     ], 
-    Orientation: 1, // Holds the orientation of the shape.
-    Color: "rgb(0, 0, 255)", // Holds the color of the objects; used to color the grid.
+    Orientation: 0, 
+    Color: "rgb(0, 0, 255)",
   },
-  shapeT:{ // Default values for T shape
+  shapeT:{ 
     Position:[
-      [[3,1],[4,1],[5,1],[4,0]], // Active position.
-      [[3,1],[4,1],[5,1],[4,0]], // Orientation 1
-      [[4,0],[4,1],[4,2],[5,1]], // Orientation 2
-      [[3,1],[4,1],[5,1],[4,2]], // Orienation 3
-      [[4,2],[4,1],[4,0],[3,1]], // Orientation 4
+      [[3,1],[4,1],[5,1],[4,0]], 
+      [[4,0],[4,1],[4,2],[5,1]], 
+      [[3,1],[4,1],[5,1],[4,2]], 
+      [[4,2],[4,1],[4,0],[3,1]], 
     ], 
-    Orientation: 1, // Holds the orientation of the shape.
-    Color: "rgb(170, 0, 255)", // Holds the color of the objects; used to color the grid.
+    Orientation: 0, 
+    Color: "rgb(170, 0, 255)", 
   },
-  shapeO:{ // Default values for O shape
+  shapeO:{ 
     Position:[
-      [[4,0],[5,0],[4,1],[5,1]], // Active position.
-      [[4,0],[5,0],[4,1],[5,1]], // Orientation 1
+      [[4,0],[5,0],[4,1],[5,1]], 
     ], 
-    Orientation: 1, // Holds the orientation of the shape.
-    Color: "rgb(255, 244, 0)", // Holds the color of the objects; used to color the grid.
+    Orientation: 0, 
+    Color: "rgb(255, 244, 0)", 
   },
-  shapeI:{ // Default values for I shape
+  shapeI:{ 
     Position:[
-      [[3,1],[4,1],[5,1],[6,1]], // Active position.
-      [[3,1],[4,1],[5,1],[6,1]], // Orientation 1
-      [[5,0],[5,1],[5,2],[5,3]], // Orientation 2
-      [[3,2],[4,2],[5,2],[6,2]], // Orientation 3
-      [[4,0],[4,1],[4,2],[4,3]], // Orientation 4
+      [[3,1],[4,1],[5,1],[6,1]], 
+      [[5,0],[5,1],[5,2],[5,3]], 
+      [[3,2],[4,2],[5,2],[6,2]], 
+      [[4,0],[4,1],[4,2],[4,3]], 
     ], 
-    Orientation: 1, // Holds the orientation of the shape.
-    Color: "rgb(166, 244, 255)", // Holds the color of the objects; used to color the grid.
+    Orientation: 0, 
+    Color: "rgb(166, 244, 255)", 
   },
-  shapeS:{ // Default values for S shape
+  shapeS:{ 
     Position:[
-      [[3,1],[4,1],[4,0],[5,0]], // Active position. 
-      [[3,1],[4,1],[4,0],[5,0]], // Orientation 1
-      [[4,0],[4,1],[5,1],[5,2]], // Orientation 2
-      [[5,1],[4,1],[4,2],[3,2]], // Orientation 3
-      [[4,2],[4,1],[3,1],[3,0]], // Orientation 4
+      [[3,1],[4,1],[4,0],[5,0]], 
+      [[4,0],[4,1],[5,1],[5,2]], 
+      [[5,1],[4,1],[4,2],[3,2]], 
+      [[4,2],[4,1],[3,1],[3,0]], 
     ], 
-    Orientation: 1, // Holds the orientation of the shape.
-    Color: "rgb(0, 247, 0)", // Holds the color of the objects; used to color the grid.
+    Orientation: 0, 
+    Color: "rgb(0, 247, 0)", 
   },
-  shapeZ:{ // Default values for Z shape
+  shapeZ:{ 
     Position:[
-      [[3,0],[4,0],[4,1],[5,1]], // Active position. 
-      [[3,0],[4,0],[4,1],[5,1]], // Orientation 1
-      [[5,0],[5,1],[4,1],[4,2]], // Orientation 2
-      [[5,2],[4,2],[4,1],[3,1]], // Orientation 3
-      [[3,2],[3,1],[4,1],[4,0]], // Orientation 4
+      [[3,0],[4,0],[4,1],[5,1]], 
+      [[5,0],[5,1],[4,1],[4,2]], 
+      [[5,2],[4,2],[4,1],[3,1]], 
+      [[3,2],[3,1],[4,1],[4,0]], 
     ], 
-    Orientation: 1, // Holds the orientation of the shape.
-    Color: "rgb(255, 0, 0)", // Holds the color of the objects; used to color the grid.
+    Orientation: 0, 
+    Color: "rgb(255, 0, 0)", 
   },
   activeShape:{},
   bgColor:"rgba( 255, 255, 255, 0.25 )",
   highestRow:21, // Used to store the highest row that's occupied to prevent unnecessarily checking a bunch of empty rows. Set to one more than the maximum row.
   upcomingGrid:[],
 }
+let gameDefaults = {};
 
 // Grid generation. Since it generates left to right we start iterating through each ROW and generate COLUMNS before moving to the next row.
 window.onload = function() {
@@ -104,20 +98,22 @@ window.onload = function() {
       game.grid[x][y] = [false,game.bgColor];
     }
   }
+  gameStateClone(1); // Sets gameDefaults to the game object after grids are generated to hold all default values for later.
 }
 
 // Key press functionality
 document.addEventListener("keydown", event => {
-  if (event.code === "ArrowLeft" && !collisionCheck(game.activeShape.Position[0],"left")) {
+  const ore = game.activeShape.Orientation;
+  const pos = game.activeShape.Position[ore];
+  if (event.code === "ArrowLeft" && !collisionCheck(pos,"left")) {
       moving("left");
-  } else if(event.code === "ArrowRight" && !collisionCheck(game.activeShape.Position[0],"right")) {
+  } else if(event.code === "ArrowRight" && !collisionCheck(pos,"right")) {
     moving("right");
-  } else if(event.code === "ArrowDown" && !collisionCheck(game.activeShape.Position[0],"fall")) {
+  } else if(event.code === "ArrowDown" && !collisionCheck(pos,"fall")) {
     moving("fall");
   } else if(event.code === "ArrowUp") {
-    flipShape("right");
+      flipShape("right");
   } else if(event.code === "Digit0") {
-    console.log("Triggered");
     flipShape("left");
   }
 });
@@ -174,63 +170,62 @@ function test2(ele) {
   console.log(`The color of this grid is ${game.grid[x][y][1]}`);
 }
 
-// Spawn testing functionality.
 function startGame() {
   if(!game.started) {
     generateUpcoming();
     spawnShape();
     game.started = true;
-  }
+  } 
 }
 
 function spawnShape() {
   game.activeShape = structuredClone(game.upcomingBlock);
   render("draw");
   game.activeShape.fallInterval = setInterval(function() {
-    if(!collisionCheck(game.activeShape.Position[0],"fall")) {
+    let ore = game.activeShape.Orientation;
+    let pos = game.activeShape.Position[ore];
+    if(!collisionCheck(pos,"fall")) {
       moving("fall");}
-  }, 500);
+  }, 500); 
   generateUpcoming();
 }
 
 function flipShape(flipDir) {
-  let newOre = 0;
+  let newOre = 0; 
   const lastOre = game.activeShape.Position.length-1;
   const currentOre = game.activeShape.Orientation;
-  if(lastOre === 1) return;
-  if(flipDir === "left" && currentOre === 1 && !collisionCheck(game.activeShape.Position[lastOre],"flip")) {
+  if(lastOre === 0) return;
+  if(flipDir === "left" && currentOre === 0 && !collisionCheck(game.activeShape.Position[lastOre],"flip")) {
     newOre = lastOre;
-  } else if(flipDir === "left" && currentOre !== 1 && !collisionCheck(game.activeShape.Position[currentOre-1],"flip")) {
+  } else if(flipDir === "left" && currentOre !== 0 && !collisionCheck(game.activeShape.Position[currentOre-1],"flip")) {
     newOre = currentOre - 1;
-  } else if(flipDir === "right" && currentOre === lastOre && !collisionCheck(game.activeShape.Position[1],"flip")) {
-    newOre = 1;
+  } else if(flipDir === "right" && currentOre === lastOre && !collisionCheck(game.activeShape.Position[0],"flip")) {
+    newOre = 0;
   } else if(flipDir === "right" && currentOre !== lastOre && !collisionCheck(game.activeShape.Position[currentOre+1],"flip")) {
     newOre = currentOre + 1;
   }
-  if(newOre !== 0) {
-    game.activeShape.Orientation = newOre;
+  if(newOre !== -1) {
     render("undraw"); // Uncolor the current position
-    for(let i=0; i<game.activeShape.Position[0].length; i++) {
-      game.activeShape.Position[0][i][0] = game.activeShape.Position[newOre][i][0];
-      game.activeShape.Position[0][i][1] = game.activeShape.Position[newOre][i][1];
-    }
+    game.activeShape.Orientation = newOre;
     render("draw"); // Color the new updated position
   }
 }
 
 // Renders or undraws the currently active shape
-function render(a) {
+function render(drawOrUndraw) {
   let renderColor;
-  if(a === "draw") {
+  let ore = game.activeShape.Orientation;
+  let pos = game.activeShape.Position[ore];
+  if(drawOrUndraw === "draw") {
     renderColor = game.activeShape.Color;
-  } else if(a === "undraw") {
+  } else if(drawOrUndraw === "undraw") {
     renderColor = game.bgColor;
   } else {
     console.log("Render function called with improper parameter.");
   }
-  for(let i=0; i<game.activeShape.Position[0].length; i++){
-    const x = game.activeShape.Position[0][i][0];
-    const y = game.activeShape.Position[0][i][1];
+  for(let i=0; i<pos.length; i++){
+    const x = pos[i][0];
+    const y = pos[i][1];
     const selector = `[data-x="${x}"][data-y="${y}"]`;
     const targetDiv = document.querySelector(selector);
     game.grid[x][y][1] = renderColor;
@@ -238,10 +233,10 @@ function render(a) {
   }
 }
 
-function renderUpcoming(b) {
-  if(b === "draw") {
+function renderUpcoming(drawOrUndraw) {
+  if(drawOrUndraw === "draw") {
     thisColor = game.upcomingBlock.Color;
-  } else if(b === "undraw") {
+  } else if(drawOrUndraw === "undraw") {
     thisColor = game.bgColor;
   }
   game.upcomingGrid.forEach(divID => {
@@ -256,20 +251,20 @@ function moving(dir) {
     for(b=0; b<game.activeShape.Position[0].length; b++){ // loops through each orientation's parts
       if(dir === "right") {
         game.activeShape.Position[i][b][0] += 1; // Sets every part of every orientation to the row right of it. i = orienation, b = part, 0 is x and 1 is y
-    } else if(dir === "left" ) {
-        game.activeShape.Position[i][b][0] -= 1; // Sets every part of every orientation to the row left of it.
-    } else if(dir === "fall") {
-        game.activeShape.Position[i][b][1] += 1; // Sets every part of every orientation to the row below it.
+      } else if(dir === "left" ) {
+          game.activeShape.Position[i][b][0] -= 1; // Sets every part of every orientation to the row left of it.
+      } else if(dir === "fall") {
+          game.activeShape.Position[i][b][1] += 1; // Sets every part of every orientation to the row below it.
+      }
     }
   }
   render("draw"); // Draws the new location after the coordinates are updated.
-  }
 }
 
-function collisionCheck(newPos, action) {
-  for(let i=0; i<newPos.length; i++) {
-    const x = newPos[i][0];
-    const y = newPos[i][1];
+function collisionCheck(checkPos, action) {
+  for(let i=0; i<checkPos.length; i++) {
+    const x = checkPos[i][0];
+    const y = checkPos[i][1];
     if(action === "fall" && (y === 19 || game.grid[x][y+1][0] === true)) {
       settleShape();
       return true; // collision is true
@@ -277,7 +272,7 @@ function collisionCheck(newPos, action) {
       return true; // collision is true
     } else if(action === "right" && (x === 9 || game.grid[x+1][y][0] === true)) {
       return true; // collision is true
-    } else if(action === "flip" && (x < 0 || x >= game.maxCol || y < 0 || y >= game.maxRow || game.grid[x][y][0] === true)) {
+    } else if(action === "flip" && (x < 0 || x >= game.maxCol-1 || y < 0 || y >= game.maxRow-1 || game.grid[x][y][0] === true)) {
       return true; // invalid flip orientation because it's either off the grid or occupies an already occupied location.
     }
   }
@@ -285,28 +280,32 @@ function collisionCheck(newPos, action) {
 }
 
 function settleShape(){
+  let ore = game.activeShape.Orientation;
+  let pos = game.activeShape.Position[ore];
   let lowestRow = game.activeShape.Position[0][0][1]; // Sets up the variable for holding the lowest row that the shape occupies.
-  for(let i=0; i<game.activeShape.Position[0].length; i++){ // Iterate through the active position's shape pieces.
-    if(game.activeShape.Position[0][i][1] > lowestRow) { // If the current piece of the shape is on a lower row than the previous
-      lowestRow = game.activeShape.Position[0][i][1]; // then update to the lowest row
+  for(let i=0; i<pos.length; i++){ // Iterate through the active position's shape pieces.
+    if(pos[i][1] > lowestRow) { // If the current piece of the shape is on a lower row than the previous
+      lowestRow = pos[i][1]; // then update to the lowest row
     }
-    if(game.activeShape.Position[0][i][1] < game.highestRow) { // If the currently checked shape piece is on a higher row than the highest logged row
-      game.highestRow = game.activeShape.Position[0][i][1]; // then update the highestRow
+    if(pos[i][1] < game.highestRow) { // If the currently checked shape piece is on a higher row than the highest logged row
+      game.highestRow = pos[i][1]; // then update the highestRow
     }
-    const x = game.activeShape.Position[0][i][0];
-    const y = game.activeShape.Position[0][i][1];
+    const x = pos[i][0];
+    const y = pos[i][1];
     game.grid[x][y][0] = true;
     game.grid[x][y][1] = game.activeShape.Color;
   }
-  console.log("Triggered");
   clearInterval(game.activeShape.fallInterval);
-  Object.keys(game.activeShape).forEach(key => delete game.activeShape[key]); // Clears the activeShape object
   checkRows(lowestRow,game.highestRow-1); // Starts checking rows starting from the lowest row the shape occupies and stops before highestRow-1.
-  spawnShape();
+  if(game.highestRow === 0) {
+    alert("You lose!");
+    gameOver();
+  } else {
+    spawnShape();
+  }
 }
 
 async function checkRows(startHere, stopHere) {
-  console.log(`Check row triggered to check row ${startHere} and stopping before row ${stopHere} `);
   for(let y=startHere; y>stopHere; y--) {
     // Use stopHere to allow recursive calling of this function while only checking some rows instead of having to iterate over them all multiple times. -1 to check all, 18 to check only bottom row.
     // for loop to iterate through each column
@@ -358,4 +357,27 @@ async function clearRows(row) { // Row is the row that needs to be cleared.
 // function to allow async and allow for a clearing animation not to interfere with the animation that brings shapes down a row
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function gameOver() {
+  game.started = false;
+  renderUpcoming("undraw");
+  gameStateClone(2); // clones the current game state from the gameDefaults object to return values to their defaults.
+  // Clear the main board.
+  for (let y = 0; y < game.maxRow; y++) {
+    for (let x = 0; x < game.maxCol; x++) {
+      const selector = `[data-x="${x}"][data-y="${y}"]`;
+      const targetDiv = document.querySelector(selector);
+      game.grid[x][y] = [false,game.bgColor];
+      targetDiv.style.backgroundColor = game.bgColor;
+    }
+  }
+}
+
+function gameStateClone(clone) {
+  if(clone === 1) {
+    gameDefaults = structuredClone(game);
+  } else if(clone === 2) {
+    game = structuredClone(gameDefaults);
+  }
 }
